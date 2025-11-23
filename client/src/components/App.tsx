5e357df6a5a64e265d1bb4c89a6abe37d636f535
pieeernik <igopie01@easv365.dk>
2025-11-23 02:33:10 +0100

    fix prices

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -8,7 +8,6 @@ function App() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/game" element={<GameBoard />} />
         </Routes>
     )
 }
