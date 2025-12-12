e0cd58cbdf1590a3f0c7939efc3bd7a30243a2cd
Stefan Rubelov <stefanrubelov@Stefans-MacBook-Pro.local>
2025-12-12 12:12:30 +0100

    base url indentation fix

--- client/src/core/baseUrl.ts
+++ client/src/core/baseUrl.ts
@@ -1,5 +1,5 @@
 const isProduction = import.meta.env.VITE_PROD;
 
- const prod = "https://projectsolutionserver.fly.dev";
- const dev = "http://localhost:5284";
+const prod = "https://projectsolutionserver.fly.dev";
+const dev = "http://localhost:5284";
 export const baseUrl = isProduction ? prod : dev;
